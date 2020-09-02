f6797df1be10315c99357667223fc80c9aa3bbf4
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-09-03 05:25:22 +0600

    updated

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -11,30 +11,32 @@ export default function SignIn() {
     <div className='joinOuterContainer'>
       <div className='joinInnerContainer'>
         <h1 className='heading'>Realtime Chat App</h1>
-        <div>
-          <input
-            placeholder='Your Name'
-            className='joinInput'
-            type='text'
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            placeholder='Room Code'
-            className='joinInput mt-20'
-            type='text'
-            onChange={(event) => setRoom(event.target.value)}
-          />
-        </div>
-        <Link
-          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className={'button mt-20'} type='submit'>
-            Sign In
-          </button>
-        </Link>
+        <form>
+          <div>
+            <input
+              placeholder='Your Name'
+              className='joinInput'
+              type='text'
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              placeholder='Room Code'
+              className='joinInput mt-20'
+              type='text'
+              onChange={(event) => setRoom(event.target.value)}
+            />
+          </div>
+          <Link
+            onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className={'button mt-20'} type='submit'>
+              Sign In
+            </button>
+          </Link>
+        </form>
       </div>
     </div>
   );
